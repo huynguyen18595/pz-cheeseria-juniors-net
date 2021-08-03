92c8a9743a42e59e3a800c9e41c718fde53b3737
STEVENGUYEN\Steve Nguyen <huynguyen18595@gmail.com>
2021-08-03 20:24:52 +1000

    Requirement 4: use Cypress.io to create an UI automation test to test Puchse function

--- client/e2e/cypress/integration/cart-actions.spec.js
+++ client/e2e/cypress/integration/cart-actions.spec.js
@@ -7,11 +7,16 @@ context('Cart Actions', () => {
 
   it.only('Add items to cart', () => {
     cy.get('[data-cy=add-to-cart-2]').click();
-    cy.get('[data-cy=add-to-cart-3]').click();
+    cy.get('[data-cy=add-to-cart-7]').click();
 
     cy.get('[data-cy=view-cart-btn]').click();
     cy.contains('2 Selected item')
-
+    cy.get('[data-cy=purchase-btn]').click();
+    cy.get('[data-cy=recent-purchase-btn]').click();
   })
 
 })
+//1. Add 2 seperate items to the cart
+
+//2. Click on purchase 
+
